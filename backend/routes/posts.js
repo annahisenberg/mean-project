fa854df535fdc72814372870611c255d0844b6ee
Annah Isenberg <annahisenberg@gmail.com>
2019-01-18 12:16:52 -0600

    add pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -64,6 +64,7 @@ router.get('', (req, res, next) => {
     const pageSize = +req.query.pagesize;
     const currentPage = +req.query.page;
     const postQuery = Post.find();
+    let fetchedPosts;
     if (pageSize && currentPage) {
       postQuery
         .skip(pageSize * (currentPage - 1))
@@ -71,9 +72,14 @@ router.get('', (req, res, next) => {
     }
     postQuery
         .then(documents => {
-            res.status(200).json({
+            fetchedPosts = documents;
+            return Post.count();
+        })
+        .then(count => {
+          res.status(200).json({
                 message: 'Posts fetched successfully',
-                posts: documents
+                posts: fetchedPosts,
+                maxPosts: count
             });
         });
 });
